0c8b99378ee32eb9c40070e3d16b1ca86b33c3a0
jmarcos00312 <jeremiahmarcos27@outlook.com>
2022-08-17 16:59:11 -0500

    rules made

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -28,6 +28,26 @@ choices = {
 }
 
 userChoice = input("rock? paper? scissors? ")
+compChoice = random.choice(list(choices.keys()))
 
-print(choices[userChoice])
+print(f"You choose: userChoice {choices[userChoice]}")
+print(f"The computer choose: userChoice {choices[compChoice]}")
 
+if userChoice == "rock" and compChoice == "paper":
+    print("Computer Wins!")
+elif userChoice == "paper" and compChoice == "paper":
+    print("DRAW")
+elif userChoice == "scissors" and compChoice == "paper":
+    print("You Win!")
+elif userChoice == "rock" and compChoice == "rock":
+    print("DRAW")
+elif userChoice == "paper" and compChoice == "rock":
+    print("You Win!")
+elif userChoice == "scissors" and compChoice == "rock":
+    print("Computer Wins!")
+elif userChoice == "rock" and compChoice == "scissors":
+    print("You Win!")
+elif userChoice == "paper" and compChoice == "scissors":
+    print("Computer Wins!")
+elif userChoice == "scissors" and compChoice == "scissors":
+    print("DRAW")
