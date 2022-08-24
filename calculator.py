8f8bc533a055f0c3b921b18bf6c686b7a82e1ea1
Jeremiah <jeremiahmarcos27@outlook.com>
2022-08-24 12:20:38 -0500

    calculator mini project done

--- calculator.py
+++ calculator.py
@@ -1,2 +1,39 @@
 def calculate():
-    pass
+    final = 0
+    wantsToCont = True
+    
+    firstNum = input("What is the number? ")
+    while wantsToCont:
+        operation = input("""Do you want to: 
+/
+*
++
+-
+""")
+        anotherNum = input("What is the number? ")
+        
+        if operation == "/":
+            if final == 0:
+                final = float(firstNum) / float(anotherNum)
+            else:
+                final /= float(anotherNum)
+        elif operation == "*":
+            if final == 0:
+                final = float(firstNum) * float(anotherNum)
+            else:
+                final *= float(anotherNum)
+        elif operation == "+":
+            if final == 0:
+                final = float(firstNum) + float(anotherNum)
+            else:
+                final += float(anotherNum)
+        elif operation == "-":
+            if final == 0:
+                final = float(firstNum) - float(anotherNum)
+            else:
+                final -= float(anotherNum)
+        cont = input(f"Do you want to calculate {final} to something else? ")
+        if cont == "no":
+            wantsToCont = False
+
+calculate()
